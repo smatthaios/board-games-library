c05297743813cda88598020ea6f49714033f6dce
Vicky <vpan@gmail.com>
2016-04-17 22:04:50 +0300

    Adding files (under the renamed folders)

---
+++ web/board-game-library-webapp/src/main/webapp/js/application/controller/boardGameManagement.js
@@ -0,0 +1,48 @@
+/**
+ * Board Game Management Controller is responsible to manipulate the Board Games of the application.
+ */
+app.controller('BoardGameManagement', ['$scope', '$rootScope',
+	function ($scope, $rootScope) {
+
+
+		/**
+		 * Creates the scope functions of the Controller.
+		 */
+		function createScopeFunctions() {
+			$scope.openBoardGameModal = function (){
+
+			}
+		}
+
+		/**
+		 * Initializes the scope functions of the Controller.
+		 */
+		function initScopeVariables() {
+
+		}
+
+		/**
+		 * Initializes actions on UI components.
+		 */
+		function setActions() {
+
+		}
+
+		/**
+		 * Initializes the Controller.
+		 */
+		function initializeController() {
+			createScopeFunctions();
+			setActions();
+			initScopeVariables();
+			//Validator.init();
+
+			$scope.rowCollection = [
+				{firstName: 'Board Game 1', lastName: 'Hasbro', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+				{firstName: 'Board Game 2', lastName: 'Hasbro', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+				{firstName: 'Board Game 3', lastName: 'Hasbro', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+			];
+		}
+
+		initializeController();
+	}]);
