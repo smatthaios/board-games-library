8147a6e513237f2f69dc9f62ef336383fd70ed69
Vicky <vpan@gmail.com>
2016-04-24 20:16:54 +0300

    [BGL-2], [BGL-3], [BGL-4]

--- parser/src/main/java/gr/boardgames/library/domain/BoardGames.java
+++ parser/src/main/java/gr/boardgames/library/domain/BoardGames.java
@@ -1,5 +1,5 @@
 package gr.boardgames.library.domain;
-import javax.xml.bind.annotation.XmlAttribute;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
